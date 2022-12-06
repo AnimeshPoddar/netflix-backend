ed78f0a41959e0ee4b666f8e9a37812652005749
Animesh <animesh.poddar@vkaps.com>
2022-12-06 16:15:47 +0530

    setup vercel

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const adminRouter = require("./routes/adminRouter");
 const userRouter = require("./routes/userRouter");
 app.use(express.json());
 
-app.use("/", (req, res) => {
-  res.send("Welcome to Netflix Backenddcsd.");
-});
 app.use("/api/v1/auth", authRouter); //Using auth router's endpoint
 app.use("/api/v1", adminRouter); //Using adminrouter's endpoint
 app.use("/api/v1", userRouter); //Using user router's endpoint
 
+app.use("/", (req, res) => {
+  res.send("Welcome to Netflix Backenddcsd.");
+});
 module.exports = app;
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -2,6 +2,6 @@ const express = require("express");
 const { getAllUsers } = require("../controllers/adminController");
 const router = express.Router();
 
-router.route("/admin/users").get(getAllUsers)
+// router.route("/admin/users").get(getAllUsers);
 
 module.exports = router;
