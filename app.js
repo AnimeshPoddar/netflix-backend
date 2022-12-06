4bfcc5d318e2575f0e5c9a43b44bc59fdae8bec3
Animesh <animesh.poddar@vkaps.com>
2022-12-06 16:34:53 +0530

    setup vercel

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/adminRouter");
 const userRouter = require("./routes/userRouter");
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1/auth", authRouter); //Using auth router's endpoint
