6a2cb5b71c741224ae7ff499f2f3bcd1d46466fd
Animesh <animesh.poddar@vkaps.com>
2022-12-06 15:11:02 +0530

    create a home route

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const adminRouter = require("./routes/adminRouter");
 const userRouter = require("./routes/userRouter");
 app.use(express.json());
 
+app.use("/",(req,res)=>{
+    res.send("Welcome to Netflix Backend.")
+})
 app.use("/api/v1/auth", authRouter); //Using auth router's endpoint
 app.use("/api/v1", adminRouter); //Using adminrouter's endpoint
 app.use("/api/v1", userRouter); //Using user router's endpoint
