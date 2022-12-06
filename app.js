248476084e4f90ea03f52c4eceb7384bb2989f10
Animesh <animesh.poddar@vkaps.com>
2022-12-06 14:53:54 +0530

    changes in app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-const authRouter = require("./routes/auth")
+const authRouter = require("./routes/auth");
 const adminRouter = require("./routes/adminRouter");
 const userRouter = require("./routes/userRouter");
-app.use(express.json()); 
+app.use(express.json());
 
 app.use("/api/v1/auth", authRouter); //Using auth router's endpoint
-app.use("/api/v1",adminRouter);//Using adminrouter's endpoint
-app.use("/api/v1",userRouter);//Using user router's endpoint
+app.use("/api/v1", adminRouter); //Using adminrouter's endpoint
+app.use("/api/v1", userRouter); //Using user router's endpoint
 
 module.exports = app;
